80459a0d56d96277f3178342baa23f71eb72ee2b
arielfernandes3 <0.arielfernandes.0@gmail.com>
2020-12-03 12:06:59 -0300

    Create Templates

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
 	<footer className="footer">
-		Rodapé
+		<span>
+			Desenvoldido com <i className="fa fa-heart text-danger"></i> por
+			<strong> Fi<span className="text-danger">s</span>h</strong>
+		</span>
 	</footer>
